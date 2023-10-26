5565aa32d0ce19086c00aebab8160b3f17e41fe7
rayaqin <rayaqin@gmail.com>
2023-10-26 02:39:39 +0200

    task 1 for 2019

---
+++ 2019/task1/solution.js
@@ -0,0 +1,64 @@
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+  console.log("File APIs are supported in your browser, you may proceed.");
+} else {
+  alert("The File APIs are not fully supported in this browser. The code won't work.");
+}
+
+const chooseFile = document.getElementById("choose-file");
+const inputLabel = document.getElementById("input-label");
+const partSelection = document.getElementById("part-selection");
+const messageToUser = document.getElementById("message-to-user");
+let fileContent;
+
+const handleFileSelect = (event) => {
+  const reader = new FileReader();
+  reader.readAsText(event.target.files[0]);
+  reader.onload = (e) => {
+    fileContent = e.target.result;
+    addPartSelectionButtons();
+  };
+};
+
+chooseFile.addEventListener("change", handleFileSelect, false);
+
+const writeMessageToUser = (message) => {
+  messageToUser.classList.contains("invisible") || messageToUser.classList.add("invisible");
+  messageToUser.innerHTML = message;
+  setTimeout(() => {
+    messageToUser.classList.remove("invisible");
+  }, 2000);
+};
+
+const addPartSelectionButtons = () => {
+  partSelection.style.visibility = "visible";
+  writeMessageToUser("select which part's result you'd like to see");
+};
+
+const solveSelectedPart = (partId) => {
+  writeMessageToUser("check the console");
+  const solution = partId === 1 ? getSolutionForPart1(fileContent) : getSolutionForPart2(fileContent);
+  console.log(`Solution for part ${partId}:`, solution);
+};
+
+const getSolutionForPart1 = (source) => {
+  return source.split('\r\n').map(line => getFuelConsumptionFromMass(parseInt(line))).reduce((acc, curr) => acc + curr, 0);
+};
+
+const getSolutionForPart2 = (source) => {
+  return source.split('\r\n').map(line => getAllFuelFromFuel(getFuelConsumptionFromMass(parseInt(line)))).reduce((acc, curr) => acc + curr, 0);
+};
+
+const getAllFuelFromFuel = (fuelMass) => {
+  let latestFuelMassData = fuelMass;
+  let allFuelForFuel = fuelMass;
+  while (latestFuelMassData > 0) {
+    latestFuelMassData = getFuelConsumptionFromMass(latestFuelMassData);
+    allFuelForFuel += latestFuelMassData > 0 ? latestFuelMassData : 0;
+  }
+  return allFuelForFuel;
+};
+
+const getFuelConsumptionFromMass = (mass) => {
+  return Math.floor(mass / 3) - 2;
+}
+
