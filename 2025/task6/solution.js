d196d2bd551b0d187930bdc8af4a7cd5c58422e0
Bálint Ujvári <rayaqin@gmail.com>
2025-12-06 14:14:07 +0100

    task6

--- 2025/task6/solution.js
+++ 2025/task6/solution.js
@@ -2,7 +2,7 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
   console.log("File APIs are supported in your browser, you may proceed.");
 } else {
   alert(
-    "The File APIs are not fully supported in this browser. The code won't work.",
+    "The File APIs are not fully supported in this browser. The code won't work."
   );
 }
 
@@ -47,8 +47,71 @@ const solveSelectedPart = (partId) => {
 };
 
 const getSolutionForPart1 = (source) => {
-  return "1";
+  const lines = source
+    .split("\n")
+    .map((line) => line.replaceAll(/\s+/g, " ").split(" ").filter(Boolean));
+
+  let combinedResults = 0;
+
+  for (let columnIndex = 0; columnIndex < lines[0].length; columnIndex++) {
+    const operator = lines.at(-1)[columnIndex];
+    let result = operator === "*" ? 1 : 0;
+    for (let rowIndex = 0; rowIndex < lines.length - 1; rowIndex++) {
+      if (operator === "*") {
+        result = result * Number(lines[rowIndex][columnIndex]);
+      } else {
+        result = result + Number(lines[rowIndex][columnIndex]);
+      }
+    }
+    combinedResults += result;
+  }
+  return combinedResults;
 };
 const getSolutionForPart2 = (source) => {
-  return "2";
+  const lines = source.split("\n");
+
+  const operators = ["*", "+"];
+  const operations = [];
+  const recentOperationData = {
+    operator: null,
+    numbers: [],
+  };
+
+  for (let columnIndex = lines[0].length - 1; columnIndex >= 0; columnIndex--) {
+    let currentColumn = "";
+    let reachedOperator = false;
+    for (let rowIndex = 0; rowIndex < lines.length; rowIndex++) {
+      const currentCharacter = lines[rowIndex][columnIndex];
+      if (operators.includes(currentCharacter)) {
+        reachedOperator = true;
+        recentOperationData.operator = currentCharacter;
+        recentOperationData.numbers.push(Number(currentColumn.trim()));
+        operations.push({
+          operator: recentOperationData.operator,
+          numbers: recentOperationData.numbers.filter((n) => n !== 0),
+        });
+        recentOperationData.numbers = [];
+      } else {
+        currentColumn += currentCharacter;
+      }
+    }
+    if (reachedOperator === false) {
+      recentOperationData.numbers.push(Number(currentColumn.trim()));
+    }
+  }
+
+  const combinedResults = operations.reduce((acc, curr) => {
+    const { numbers, operator } = curr;
+    let result = operator === "*" ? 1 : 0;
+    for (let number of numbers) {
+      if (operator === "*") {
+        result = result * number;
+      } else {
+        result = result + number;
+      }
+    }
+    return acc + result;
+  }, 0);
+
+  return combinedResults;
 };
